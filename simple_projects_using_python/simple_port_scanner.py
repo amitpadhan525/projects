4e796aaf931ba5d23cce7357121bb2e16ee3628e
amit <amit@localhost.localdomain>
2025-08-05 13:43:49 +0530

    intial commit 2025-08-05 13:43:49

--- simple_projects_using_python/simple_port_scanner.py
+++ simple_projects_using_python/simple_port_scanner.py
@@ -1,12 +1,14 @@
 import socket
 
-ip =input("Enter ip address : ")
+ip =input("Enter target ip address : ")
 starting_port=int(input("Enter starting port : "))
 ending_port =int(input("Enter endin port : "))
+
 for i in range(starting_port,ending_port):
     s=socket.socket()
     s.settimeout(0.5)
     result=s.connect_ex((ip,i))
+    
     if result == 0:
         print(f"Port {i} is open\n")
     s.close()
